d1afd1df540c0f4f293e61c78bfc7a70685ec799
jiheyon <dkgus5988@gmail.com>
2022-12-24 14:06:17 +0900

    #빌드, DB연결 변경 2( demo만)

--- damo/src/main/java/com/example/demo/HealthyCheckController/HealthyCheckController.java
+++ damo/src/main/java/com/example/demo/HealthyCheckController/HealthyCheckController.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // 백엔드 서버가 살아있는지 확인
 @RestController
 @Slf4j
+@CrossOrigin
 public class HealthyCheckController {
 
     @GetMapping("/")
@@ -15,4 +17,11 @@ public class HealthyCheckController {
         log.info("server is running...");
         return "server is running....";
     }
+
+    @GetMapping("/hello123")
+    public String check123() {
+        log.info("hello123...");
+        return "안뇽안뇽 하하호호~~";
+    }
+
 }
